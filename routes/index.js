401c4c8390571b1bdaa279ad0aa055329fb41f66
anu-007 <anubhutiv1@gmail.com>
2016-09-01 20:08:18 +0530

    new2

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 var router=express.Router();
 
-
+router.use('/uploads', express.static(__dirname + '/../uploads'));
 
 router.get('/',function(req,res){
 	res.render('index');
