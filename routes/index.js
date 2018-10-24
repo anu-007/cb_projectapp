404c7c75cd2322bcd9ebf027011555b8029528c9
Avinash <avinashb97@gmail.com>
2018-10-24 23:32:04 +0530

    Improve Code Style

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,15 @@
-const express=require('express');
-var db = require('../dbmodel/dbhandler');
-var router=express.Router();
+const express = require("express");
+var db = require("../dbmodel/dbhandler");
+var router = express.Router();
 
-router.use('/uploads', express.static(__dirname + '/../uploads'));
+router.use("/uploads", express.static(__dirname + "/../uploads"));
 
-router.get('/',function(req,res){
-	db.gettop(function(err,data){
-       if(err)
-       	console.log(err);
-       var dta=data;
-       res.render('index',{dta:dta});
-	})
-})
+router.get("/", function(req, res) {
+  db.gettop(function(err, data) {
+    if (err) console.log(err);
+    var dta = data;
+    res.render("index", { dta: dta });
+  });
+});
 
-module.exports=router;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,100 +1,84 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
-var expressValidator = require('express-validator');
-var flash = require('connect-flash');
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var mysql=require('mysql');
-var md5=require('md5');
-var path=require('path');
-var handlebars=require('handlebars');
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var expressValidator = require("express-validator");
+var flash = require("connect-flash");
+var session = require("express-session");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var mysql = require("mysql");
+var md5 = require("md5");
+var path = require("path");
+var handlebars = require("handlebars");
 
+var routes = require("./routes/index");
+var users = require("./routes/users");
 
-var routes=require('./routes/index');
-var users=require('./routes/users');
+const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs({ defaultLayout: "layout" }));
+app.set("view engine", "handlebars");
 
-
-const app=express();
-
-
-
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout:'layout'}));
-app.set('view engine', 'handlebars');
-
-           handlebars.registerHelper('ifCond', function(v1, v2, options) {
-                if(v1 === v2) {
-                 return options.fn(this);
-                 }
-               return options.inverse(this);
-             });
+handlebars.registerHelper("ifCond", function(v1, v2, options) {
+  if (v1 === v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use("/", express.static("./public"));
 
-app.use('/', express.static('./public'));
-
-
-
-app.use(session({
-    secret: 'secret',
+app.use(
+  session({
+    secret: "secret",
     saveUninitialized: true,
     resave: true
-}));
-
-
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
 
-
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
-
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
     }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
-
-
+  })
+);
 
 app.use(flash());
 
-
-
-app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   res.locals.user = req.user || null;
   next();
 });
 
+app.use("/", routes);
+app.use("/users", users);
 
+app.set("port", process.env.PORT || 5555);
 
-app.use('/', routes);
-app.use('/users', users);
-
-app.set('port', process.env.PORT || 5555);
-
-app.listen(5555, function(){
-  console.log('Server started on port 5555');
+app.listen(5555, function() {
+  console.log("Server started on port 5555");
 });
-
-
